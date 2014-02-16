4b1067e6c4b09f66ddcbb5c7f2f7efdd54011311
Owen Smith <owen@omsmith.ca>
2014-02-15 22:42:59 -0500

    Revert "switch to st as a terminal emulator"
    This reverts commit 3da8a140fbdd008a085e132af19c410bdaafa48e.

--- .bashrc
+++ .bashrc
@@ -85,5 +85,3 @@ alias dotgit="git --work-tree=$HOME --git-dir=$HOME/dev/dotfiles.git"
 #alias play="mplayer -cache 25000 -cache-min 99 -af volnorm=2 -vf pp=ac/fd"
 alias play="mplayer -cache 25000 -cache-min 99 -vf pp=ac/fd"
 
-printf "\033?\033=" >/dev/tty
-
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -25,7 +25,7 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "st"
+myTerminal      = "urxvt"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
